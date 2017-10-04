6812a53a4fe912cfe95783fb6b57dd4a4e69f550
mcaputto <mcaputto@users.noreply.github.com>
2017-10-03 18:50:58 -0600

    Rename components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import './skeleton.css'
 import './normalize.css'
-// import logo from './logo.svg';
-// import './App.css';
 
 
 const title = 'Hi, welcome to the PF calculator 2000. Beep boop.'
 const subtitle = 'Personal finance is the financial management which an individual or a family unit performs to budget, save, and spend monetary resources over time, taking into account various financial risks and future life events. When planning personal finances, the individual would consider the suitability to his or her needs of a range of banking products (checking, savings accounts, credit cards and consumer loans) or investment private equity, (stock market, bonds, mutual funds) and insurance (life insurance, health insurance, disability insurance) products or participation and monitoring of and- or employer-sponsored retirement plans, social security benefits, and income tax management'
-const salary = 'A wage is monetary compensation paid by an employer to an employee in exchange for work done.'
 const bonus = 'A bonus payment is usually made to employees in addition to their base salary as part of their wages or salary.'
 const aggregate = 'Together, guaranteed and variable pay comprise total cash compensation. The ratio of base salary to variable pay is referred to as the pay mix.'
+
+const salary = {
+    name: 'Salary',
+    description: 'A wage is monetary compensation paid by an employer to an employee in exchange for work done.',
+}
+
 
 class Header extends Component {
     render() {
@@ -46,16 +49,16 @@ const Body = () =>
     <div className="row">
         <h4>Inputs...</h4>
         <div className="row">
-            <Row description={<Description text={salary}/>} amount={<Combo message='Salary' dollars='$100,000' />} />
-            <Row description={<Description text={bonus}/>} amount={<Combo message='Bonus' dollars='$20,000' />} />
+            <CalculationRow description={<DescriptionBox text={salary.description}/>} amount={<CalculationBox message={salary.name} dollars='$100,000' />} />
+            <CalculationRow description={<DescriptionBox text={bonus}/>} amount={<CalculationBox message='Bonus' dollars='$20,000' />} />
         </div>
         <h4>Outputs...</h4>
         <div className="row">
-            <Row description={<Description text={aggregate} />} amount={<Combo message='Aggregate' dollars='$120,000' />} />
+            <CalculationRow description={<DescriptionBox text={aggregate} />} amount={<CalculationBox message='Aggregate' dollars='$120,000' />} />
         </div>
     </div>
 
-class Description extends Component {
+class DescriptionBox extends Component {
     render() {
         return (
             <div className="six columns" style={{textAlign: 'left'}}>
@@ -65,7 +68,7 @@ class Description extends Component {
     }
 }
 
-class Combo extends Component {
+class CalculationBox extends Component {
     render () {
         return (
             <div className="six columns">
@@ -78,7 +81,7 @@ class Combo extends Component {
     }
 }
 
-class Row extends Component {
+class CalculationRow extends Component {
     render() {
         return (
             <div className="row">
