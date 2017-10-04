8085a92b9bdbf319c568c71b4bb379aafd113d50
mcaputto <mcaputto@users.noreply.github.com>
2017-10-03 19:18:02 -0600

    Combined constants in objects

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import './skeleton.css'
 import './normalize.css'
 
 
-const title = 'Hi, welcome to the PF calculator 2000. Beep boop.'
-const subtitle = 'Personal finance is the financial management which an individual or a family unit performs to budget, save, and spend monetary resources over time, taking into account various financial risks and future life events. When planning personal finances, the individual would consider the suitability to his or her needs of a range of banking products (checking, savings accounts, credit cards and consumer loans) or investment private equity, (stock market, bonds, mutual funds) and insurance (life insurance, health insurance, disability insurance) products or participation and monitoring of and- or employer-sponsored retirement plans, social security benefits, and income tax management'
-const bonus = 'A bonus payment is usually made to employees in addition to their base salary as part of their wages or salary.'
-const aggregate = 'Together, guaranteed and variable pay comprise total cash compensation. The ratio of base salary to variable pay is referred to as the pay mix.'
+const header = {
+    title: 'Hi, welcome to the PF calculator 2000. Beep boop.',
+    subtitle: 'Personal finance is the financial management which an individual or a family unit performs to budget, save, and spend monetary resources over time, taking into account various financial risks and future life events. When planning personal finances, the individual would consider the suitability to his or her needs of a range of banking products (checking, savings accounts, credit cards and consumer loans) or investment private equity, (stock market, bonds, mutual funds) and insurance (life insurance, health insurance, disability insurance) products or participation and monitoring of and- or employer-sponsored retirement plans, social security benefits, and income tax management',
+}
 
-const salary = {
-    name: 'Salary',
-    description: 'A wage is monetary compensation paid by an employer to an employee in exchange for work done.',
+const metrics = {
+    salary: {
+        name: 'Salary',
+        description: 'A wage is monetary compensation paid by an employer to an employee in exchange for work done.',
+    },
+    bonus: {
+        name: 'Bonus',
+        description: 'A bonus payment is usually made to employees in addition to their base salary as part of their wages or salary.',
+    },
+    aggregate: {
+        name: 'Aggregate',
+        description: 'Together, guaranteed and variable pay comprise total cash compensation. The ratio of base salary to variable pay is referred to as the pay mix.',
+    },
 }
 
 
@@ -38,8 +48,8 @@ class Title extends Component {
     render() {
         return (
             <div className="row">
-                <Header phrase={title} />
-                <HeaderDescription phrase={subtitle}/>
+                <Header phrase={header.title} />
+                <HeaderDescription phrase={header.subtitle}/>
             </div>
         )
     }
@@ -49,12 +59,12 @@ const Body = () =>
     <div className="row">
         <h4>Inputs...</h4>
         <div className="row">
-            <CalculationRow description={<DescriptionBox text={salary.description}/>} amount={<CalculationBox message={salary.name} dollars='$100,000' />} />
-            <CalculationRow description={<DescriptionBox text={bonus}/>} amount={<CalculationBox message='Bonus' dollars='$20,000' />} />
+            <CalculationRow description={<DescriptionBox text={metrics.salary.description}/>} amount={<CalculationBox message={metrics.salary.name} dollars='$100,000' />} />
+            <CalculationRow description={<DescriptionBox text={metrics.bonus.description}/>} amount={<CalculationBox message={metrics.bonus.name} dollars='$20,000' />} />
         </div>
         <h4>Outputs...</h4>
         <div className="row">
-            <CalculationRow description={<DescriptionBox text={aggregate} />} amount={<CalculationBox message='Aggregate' dollars='$120,000' />} />
+            <CalculationRow description={<DescriptionBox text={metrics.aggregate.description} />} amount={<CalculationBox message={metrics.aggregate.name} dollars='$120,000' />} />
         </div>
     </div>
 
@@ -124,4 +134,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default App
