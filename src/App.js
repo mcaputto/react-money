a261ba88e20bab3d26ee250ff4bfc41addf289fc
mcaputto <mcaputto@users.noreply.github.com>
2017-10-04 23:22:55 -0600

    KISS more

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import './skeleton.css'
 import './normalize.css'
 
 //-DATA-----------------------------------------------------------------------
-const metrics = {
+
+const data = {
     salary: {
         name: 'Salary',
         description: 'A wage is monetary compensation paid by an employer to an employee in exchange for work done.',
+        amount: '$100,000',
     },
     bonus: {
         name: 'Bonus',
         description: 'A bonus payment is usually made to employees in addition to their base salary as part of their wages or salary.',
+        amount: '$20,000',
     },
     totalCashCompensation: {
         name: 'Total cash compensation',
         description: 'Together, guaranteed and variable pay comprise total cash compensation. The ratio of base salary to variable pay is referred to as the pay mix.',
+        amount: '$120,000',
     },
 }
+
 // ---------------------------------------------------------------------------
 
 // -HEADER--------------------------------------------------------------------
@@ -26,7 +31,7 @@ const header = {
     subtitle: 'Personal finance is the financial management which an individual or a family unit performs to budget, save, and spend monetary resources over time, taking into account various financial risks and future life events.',
 }
 
-class WebsiteHeader extends Component {
+class Header extends Component {
     render() {
         return (
             <div className="row">
@@ -40,54 +45,24 @@ class WebsiteHeader extends Component {
         )
     }
 }
-// ---------------------------------------------------------------------------
+
 // -BODY----------------------------------------------------------------------
-const WebsiteBody = () =>
+
+const Body = () =>
     <div className="row">
-        <h4>Please enter your inputs here...</h4>
         <div className="row">
-            <AppRow
-                description={
-                    <DescriptionArea
-                        text={metrics.salary.description}
-                    />
-                }
-                amount={
-                    <InputArea
-                        metricName={metrics.salary.name}
-                        metricValue='$100,000'
-                    />
-                }
+            <AppRow metricName={data.salary.name}
+                    metricValue={data.salary.amount}
+                    metricText={data.salary.description}
             />
-            <AppRow
-                description={
-                    <DescriptionArea
-                        text={metrics.bonus.description}
-                    />
-                }
-                amount={
-                    <InputArea
-                        metricName={metrics.bonus.name}
-                        metricValue='$20,000'
-                    />
-                }
+            <AppRow metricName={data.bonus.name}
+                    metricValue={data.bonus.amount}
+                    metricText={data.bonus.description}
             />
-        </div>
-        <h4>Here are your personal finance metrics...</h4>
-        <div className="row">
-            <AppRow
-                description={
-                    <DescriptionArea
-                        text={metrics.totalCashCompensation.description}
+            <AppRow metricName={data.totalCashCompensation.name}
+                    metricValue={data.totalCashCompensation.amount}
+                    metricText={data.totalCashCompensation.description}
                     />
-                }
-                amount={
-                    <InputArea
-                        metricName={metrics.totalCashCompensation.name}
-                        metricValue='$120,000'
-                    />
-                }
-            />
         </div>
     </div>
 
@@ -95,57 +70,36 @@ class AppRow extends Component {
     render() {
         return (
             <div className="row">
-                {this.props.amount}
-                {this.props.description}
-            </div>
-        )
-    }
-}
-
-class DescriptionArea extends Component {
-    render() {
-        return (
-            <div className="one-half column" style={{textAlign: 'left'}}>
-                <p>{this.props.text}</p>
-            </div>
-        )
-    }
-}
-
-class InputArea extends Component {
-    render () {
-        return (
-            <div className="one-half column">
-                <div className="row">
-                    <SubInputArea text={<strong>{this.props.metricName}</strong>} />
-                    <SubInputArea text={this.props.metricValue} />
+                <div className="one-half column">
+                    <div className="row">
+                        <div className="one-half column" style={{textAlign: 'left'}}>
+                            <p>{<strong>{this.props.metricName}</strong>}</p>
+                        </div>
+                        <div className="one-half column" style={{textAlign: 'left'}}>
+                            <p>{this.props.metricValue}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="one-half column" style={{textAlign: 'left'}}>
+                    <p>{this.props.metricText}</p>
                 </div>
             </div>
         )
     }
 }
 
-class SubInputArea extends Component {
-    render() {
-        return (
-            <div className="one-half column" style={{textAlign: 'left'}}>
-                <p>{this.props.text}</p>
-            </div>
-        )
-    }
-}
-// ---------------------------------------------------------------------------
+
 // -MOUNT---------------------------------------------------------------------
+
 class App extends Component {
     render() {
         return (
             <div className="container">
-                <WebsiteHeader />
-                <WebsiteBody />
+                <Header />
+                <Body />
             </div>
         )
     }
 }
 
 export default App
-// ---------------------------------------------------------------------------
