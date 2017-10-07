e5e61da0b7e1107b37b1a4acadab9adfb8d9e228
mcaputto <mcaputto@users.noreply.github.com>
2017-10-07 02:02:54 -0600

    Removed styles

--- src/App.js
+++ src/App.js
@@ -1,85 +1,70 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import './skeleton.css'
-import './normalize.css'
 
 const title = 'Hi, welcome to the PF calculator 2000.'
 const subtitle = 'Personal finance is the financial management which an individual or a family unit performs to budget, save, and spend monetary resources over time, taking into account various financial risks and future life events.'
 const data = {
   salary: {
     name: 'Salary',
-    value: '$100,000',
     description: 'A wage is monetary compensation paid by an employer to an employee in exchange for work done.',
   },
   bonus: {
     name: 'Bonus',
-    value: '$20,000',
     description: 'A bonus payment is usually made to employees in addition to their base salary as part of their wages or salary.',
   },
   grossIncome: {
     name: 'Gross income',
-    value: '$120,000',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   deductions: {
     name: 'Deductions',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   exemptions: {
     name: 'Exemptions',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   adjustedGrossIncome: {
     name: 'Adjusted gross income',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   pretaxInvestment: {
     name: 'Pretax investment',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   taxableIncome: {
     name: 'Taxable income',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   ficaTax: {
     name: 'FICA tax',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   federalIncomeTax: {
     name: 'Federal income tax',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   stateIncomeTax: {
     name: 'State income tax',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   posttaxInvestment: {
     name: 'Posttax investment',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
   discretionaryIncome: {
     name: 'Discretionary income',
-    value: '$ to do...',
-    description: 'To do...',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id viverra libero. Quisque in sem sit amet ipsum feugiat tincidunt.',
   },
 }
 const footer = () =>
-  <div className="twelve columns"><p>Made by <a href='https://github.com/mcaputto'>mcaputto</a></p></div>
+  <div><p>Made by <a href='https://github.com/mcaputto'>mcaputto</a></p></div>
 
 class Header extends Component {
   render() {
     return [
-        <div className="twelve columns"><h1>{this.props.title}</h1></div>,
-        <div className="twelve columns"><p>{this.props.subtitle}</p></div>,
+        <div><h1>{this.props.title}</h1></div>,
+        <div><p>{this.props.subtitle}</p></div>,
     ]
   }
 }
@@ -107,18 +92,18 @@ class Metric extends Component {
 
   render() {
     return (
-        <div className="row">
-          <div className="three columns"><p><strong>{this.props.name}</strong></p></div>
-          <div className="three columns"><p><input type="number" value={this.state.value} onChange={this.handleChange} /></p></div>
-          <div className="six columns"><p>{this.props.description}</p></div>
+        <div>
+          <div><p><strong>{this.props.name}</strong></p></div>
+          <div><p><input type="number" value={this.state.value} onChange={this.handleChange} /></p></div>
+          <div><p>{this.props.description}</p></div>
         </div>
     )
   }
 }
 Metric.propTypes = {
   name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
 }
 Metric.defaultProps = {
   name: 'Name goes here',
@@ -129,7 +114,7 @@ Metric.defaultProps = {
 class Body extends Component {
   render() {
     return [
-      <div className="row">{this.props.items}</div>,
+      <div>{this.props.items}</div>,
     ]
   }
 }
@@ -139,22 +124,22 @@ Body.propTypes = {
 
 const generateRow = () => Object.keys(data).map(key => (
   <Metric
+    key={key}
     name={data[key].name}
     value={data[key].value}
     description={data[key].description}
-    key={key}
   />
 ))
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
         {/*Warning ignored due to https://reactjs.org/blog/2017/09/26/react-v16.0.html*/}
-        <div className="row"><Header title={title} subtitle={subtitle} /></div>
+        <div><Header title={title} subtitle={subtitle} /></div>
         {/*Warning ignored due to https://reactjs.org/blog/2017/09/26/react-v16.0.html*/}
         <Body items={generateRow()} />
-        <div className="row">{footer()}</div>
+        <div>{footer()}</div>
       </div>
     )
   }
