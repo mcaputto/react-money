ad789e7611de74e0dd083c16710b4fe62289d800
mcaputto <mcaputto@users.noreply.github.com>
2017-10-08 15:32:25 -0600

    New styling, improved descriptions, working state

--- src/App.js
+++ src/App.js
@@ -1,84 +1,38 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const title = 'PF calculator'
-const subtitle = 'Learn where your money went'
-const data = {
+const userInputsData = {
 	salary: {
 		name: 'Salary',
-		description: 'How much you make',
+		description: 'Annual earnings',
 	},
 	bonus: {
 		name: 'Bonus',
-		description: 'Cha ching!',
-	},
-	grossIncome: {
-		name: 'Gross income',
-		description: 'Very nice',
+		description: 'One-time cash bonus',
 	},
 	deductions: {
 		name: 'Deductions',
-		description: 'That\'s it?',
+		description: 'For tax year 2017 single taxpayers, this is $6,350 ',
 	},
 	exemptions: {
 		name: 'Exemptions',
-		description: 'Wish there were more...',
-	},
-	adjustedGrossIncome: {
-		name: 'Adjusted gross income',
-		description: 'The (in)famous AGI',
+		description: 'For tax year 2016, this is $4,050',
 	},
 	pretaxInvestment: {
 		name: 'Before-tax investments',
 		description: 'Max out that 401k, son!',
 	},
-	taxableIncome: {
-		name: 'Taxable income',
-		description: 'Wish this was smaller',
-	},
-	ficaTax: {
-		name: 'FICA tax',
-		description: 'Gotta take care of them old peeps',
-	},
-	federalIncomeTax: {
-		name: 'Federal income tax',
-		description: 'Uncle Sam needs his money',
-	},
-	stateIncomeTax: {
-		name: 'State income tax',
-		description: 'The state needs their share, of course',
-	},
 	posttaxInvestment: {
 		name: 'After-tax investments',
 		description: 'Max out that Roth IRA!',
 	},
-	discretionaryIncome: {
-		name: 'Discretionary income',
-		description: 'That\'s it?',
-	},
-}
-const footer = () =>
-	<div><p>Made by <a href='https://github.com/mcaputto'>mcto</a></p></div>
-
-class Header extends Component {
-	render() {
-		return [
-			<div className="title">{this.props.title}</div>,
-			<div className="subtitle">{this.props.subtitle}</div>,
-		]
-	}
-}
-
-Header.propTypes = {
-	title: PropTypes.string,
-	subtitle: PropTypes.string,
 }
 
 class Body extends Component {
 	render() {
-		return (
-			<div>{this.props.array}</div>
-		)
+		return [
+			<div className='container'>{this.props.array}</div>,
+		]
 	}
 }
 Body.propTypes = {
@@ -88,21 +42,30 @@ Body.propTypes = {
 class Metric extends Component {
 	constructor(props) {
 		super(props)
-		this.state = {value: '',}
-		this.handleChange = this.handleChange.bind(this);
+		this.state = {}
+		this.handleInputChange = this.handleInputChange.bind(this);
 	}
 
-	handleChange(event) {
-		this.setState({value: event.target.value})
+	handleInputChange(event) {
+		this.setState({
+			value: event.target.value
+		})
 	}
 
 	render() {
+// <div> debug: this.state.value is {this.state.value}
 		return (
-			<div>
-				<div><b>{this.props.name}</b></div>
-				<div>{this.props.description}</div>
-				<div><input type="number" value={this.state.value} onChange={this.handleChange} /></div>
-				<br/>
+			<div className='field'>
+				<label className='label'>{this.props.name}</label>
+				<div className='control has-icons-left'>
+				<span className='icon is-small is-left'><i className='fa fa-money'></i></span>
+					<input className='input is primary'
+						placeholder='Enter your value here...'
+						type='text'
+						onChange={this.handleInputChange}
+					/>
+				</div>
+				<p className='help'>{this.props.description}</p>
 			</div>
 		)
 	}
@@ -110,31 +73,48 @@ class Metric extends Component {
 Metric.propTypes = {
 	name: PropTypes.string.isRequired,
 	description: PropTypes.string.isRequired,
-	value: PropTypes.number,
+	// value: PropTypes.number,
 }
 Metric.defaultProps = {
 	name: 'Name goes here',
 	description: 'Description goes here',
 }
 
-const generateMetrics = () => Object.keys(data).map(key => (
+const generateUserInputRows = () => Object.keys(userInputsData).map(key => (
 	<Metric
 		key={key}
-		name={data[key].name}
-		description={data[key].description}
+		name={userInputsData[key].name}
+		description={userInputsData[key].description}
 	/>
 ))
 
 // Warnings ignored due to https://reactjs.org/blog/2017/09/26/react-v16.0.html
 class App extends Component {
 	render() {
-		return (
-			<div>
-				<Header title={title} subtitle={subtitle} />
-				<Body array={generateMetrics()} />
-				{footer()}
-			</div>
-		)
+		return [
+			<h1 className='title'>Simple finance calculator</h1>,
+			<p className='subtitle'>A financial calculator for the rest of us</p>,
+
+			<p>Enter your earnings and savings below:</p>,
+			<br/>,
+			<Body array={generateUserInputRows()} />,
+			<br/>,
+
+			<footer className='footer'>
+				<div className='container'>
+					<div className='content has-text-centered'>
+						<p>
+							<strong>Simple finance</strong> made by <a href='https://github.com/mcaputto'>mcto</a>
+						</p>
+						<p>
+							<a className='icon' href='https://github.com/mcaputto'>
+								<i className='fa fa-github'></i>
+							</a>
+						</p>
+					</div>
+				</div>
+			</footer>,
+		]
 	}
 }
 
