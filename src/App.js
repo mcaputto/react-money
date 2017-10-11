87cc72ec73add582a8a991242758e416c5ca5e7b
mcaputto <mcaputto@users.noreply.github.com>
2017-10-10 20:23:24 -0600

    Convert default component classes to presentational components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 
-const userInputsData = {
+const data = {
   earnings: {
     title: 'Earnings',
     description: 'Include all cash compensation.',
@@ -53,11 +53,11 @@ class Form extends Component {
   }
 }
 
-// const Forms = () => Object.keys(userInputsData).map(key => (
+// const Forms = () => Object.keys(data).map(key => (
 //   <Form
 //     key={key}
-//     title={userInputsData[key].title}
-//     description={userInputsData[key].description}
+//     title={data[key].title}
+//     description={data[key].description}
 //   />
 // ))
 
@@ -94,96 +94,99 @@ class Calculator extends Component {
   }
 
   render() {
-    return [
-      <div className="column is-one-third">
-        <h2 className="title is-2">Situation</h2>
-        <Form
-          title={userInputsData['earnings'].title}
-          description={userInputsData['earnings'].description}
-          propsValue={this.state.earnings}
-          onChange={this._handleEarningsChange}
-        />
-        <Form
-          title={userInputsData['before'].title}
-          description={userInputsData['before'].description}
-          propsValue={this.state.before}
-          onChange={this._handleBeforeChange}
-        />
-        <Form
-          title={userInputsData['after'].title}
-          description={userInputsData['after'].description}
-          propsValue={this.state.after}
-          onChange={this._handleAfterChange}
-        />
-        <Form
-          title={userInputsData['deductions'].title}
-          description={userInputsData['deductions'].description}
-          propsValue={this.state.deductions}
-          onChange={this._handleDeductionsChange}
-        />
-        <Form
-          title={userInputsData['exemptions'].title}
-          description={userInputsData['exemptions'].description}
-          propsValue={this.state.exemptions}
-          onChange={this._handleExemptionsChange}
-        />
-      </div>,
-      <div className="column">
-        <h2 className="title is-2">Outcome</h2>
-        <p>According to my super smart computer brain...</p>
-        <br/>
-        <ul>
-          <li>Your <strong>gross income</strong> is: {this.state.earnings}</li>
-          <li>Your <strong>adjusted gross income</strong> is: {this.state.earnings - this.state.deductions - this.state.exemptions}</li>
-          <li>Your <strong>taxable income</strong> is: {this.state.earnings - this.state.deductions - this.state.exemptions - this.state.before}</li>
+    return (
+      <div className="columns">
+        <div className="column is-one-third">
+          <h2 className="title is-2">Situation</h2>
+          <Form
+            title={data['earnings'].title}
+            description={data['earnings'].description}
+            propsValue={this.state.earnings}
+            onChange={this._handleEarningsChange}
+          />
+          <Form
+            title={data['before'].title}
+            description={data['before'].description}
+            propsValue={this.state.before}
+            onChange={this._handleBeforeChange}
+          />
+          <Form
+            title={data['after'].title}
+            description={data['after'].description}
+            propsValue={this.state.after}
+            onChange={this._handleAfterChange}
+          />
+          <Form
+            title={data['deductions'].title}
+            description={data['deductions'].description}
+            propsValue={this.state.deductions}
+            onChange={this._handleDeductionsChange}
+          />
+          <Form
+            title={data['exemptions'].title}
+            description={data['exemptions'].description}
+            propsValue={this.state.exemptions}
+            onChange={this._handleExemptionsChange}
+          />
+        </div>
+        <div className="column">
+          <h2 className="title is-2">Outcome</h2>
+          <p>According to my super smart computer brain...</p>
           <br/>
-          <li>Your <strong>FICA tax expense</strong> is:{this.state.earnings  * (.0145 +.062)}</li>
-          <li>Your <strong>federal income tax expense</strong> is:</li>
-          <li>Your <strong>state income tax expense</strong> is:</li>
-          <br/>
-          <li>Your <strong>effective FICA tax rate</strong> is:</li>
-          <li>Your <strong>effective federal income tax rate</strong> is:</li>
-          <li>Your <strong>effective state income tax rate</strong> is:</li>
-          <br/>
-          <li>Your <strong>net income spend percentage</strong> is:</li>
-          <li>Your <strong>net income save percentage</strong> is:</li>
-          <li>Your <strong>net income tax percentage</strong> is:</li>
-          <br/>
-          <li>Your <strong>gross income spend percentage</strong> is:</li>
-          <li>Your <strong>gross income save percentage</strong> is:</li>
-        </ul>
-      </div>,
-    ]
+          <ul>
+            <li>Your <strong>gross income</strong> is: {this.state.earnings}</li>
+            <li>Your <strong>adjusted gross income</strong> is: {this.state.earnings - this.state.deductions - this.state.exemptions}</li>
+            <li>Your <strong>taxable income</strong> is: {this.state.earnings - this.state.deductions - this.state.exemptions - this.state.before}</li>
+            <br/>
+            <li>Your <strong>FICA tax expense</strong> is:{this.state.earnings  * (.0145 +.062)}</li>
+            <li>Your <strong>federal income tax expense</strong> is:</li>
+            <li>Your <strong>state income tax expense</strong> is:</li>
+            <br/>
+            <li>Your <strong>effective FICA tax rate</strong> is:</li>
+            <li>Your <strong>effective federal income tax rate</strong> is:</li>
+            <li>Your <strong>effective state income tax rate</strong> is:</li>
+            <br/>
+            <li>Your <strong>net income spend percentage</strong> is:</li>
+            <li>Your <strong>net income save percentage</strong> is:</li>
+            <li>Your <strong>net income tax percentage</strong> is:</li>
+            <br/>
+            <li>Your <strong>gross income spend percentage</strong> is:</li>
+            <li>Your <strong>gross income save percentage</strong> is:</li>
+          </ul>
+        </div>
+      </div>
+    )
   }
+}
+
+const Header = () => {
+  return [
+    <h1 className='title is-1'>Simple finance</h1>,
+    <p className='subtitle is-1'>A financial calculator for the rest of us</p>,
+    <p>Hi, I'm a computer program running in your browser. All calculations are being performed client-side using your browser's JavaScript interpreter. Absolutely no data is being stored online. In fact, you could turn off your internet connection, right now, without any interruptions to my functionality. I'm here to help you learn about your finances... please fill out the text forms below to see what I can do!</p>,
+    <br/>,
+  ]
+}
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='container'>
+        <div className='content has-text-centered'>
+          <p><strong>Simple finance</strong> by <a href='https://github.com/mcaputto'>mcto</a></p>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 // Warnings ignored due to https://reactjs.org/blog/2017/09/26/react-v16.0.html
 class App extends Component {
   render() {
     return [
-      <h1 className='title is-1'>Simple finance</h1>,
-      <p className='subtitle is-1'>A financial calculator for the rest of us</p>,
-      <p>Hi, I'm a computer program running in your browser. All calculations are being performed client-side using your browser's JavaScript interpreter. Absolutely no data is being stored online. In fact, you could turn off your internet connection, right now, without any interruptions to my functionality. I'm here to help you learn about your finances... please fill out the text forms below to see what I can do!</p>,
-      <p><br/></p>,
-      <div className="columns">
-        <Calculator />
-      </div>,
-      <footer className='footer'>
-        <div className='container'>
-          <div className='content has-text-centered'>
-            <p><strong>Simple finance</strong></p>
-            <p>Made by <a href='https://github.com/mcaputto'>mcto</a></p>
-            <a href='https://nodejs.org/en/'>Node</a><br/>
-            <a href='https://www.npmjs.com'>NPM</a><br/>
-            <a href='https://yarnpkg.com/en/'>Yarn</a><br/>
-            <a href='https://webpack.github.io/'>Webpack</a><br/>
-            <a href='https://reactjs.org'>React</a><br/>
-            <a href='http://babeljs.io'>Babel</a><br/>
-            <a href='http://www.ecma-international.org/ecma-262/6.0/index.html'>ES6</a><br/>
-            <a href='https://eslint.org'>ESLint</a><br/>
-          </div>
-        </div>
-      </footer>,
+      <Header />,
+      <Calculator />,
+      <Footer />,
     ]
   }
 }
