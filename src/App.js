c59fb7d2f35e6c5b8e8c7220a70cf7e332b359d4
mcaputto <mcaputto@users.noreply.github.com>
2017-10-12 22:43:11 -0600

    Fixed indent

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ const Header = () =>
 const Introduction = () =>
 	<div>
 		<br/>
-		<p>Hi, I'm a computer program running in your browser. All calculations are
-		being performed client-side using your browser's JavaScript interpreter.
-		Absolutely no data is being stored online. In fact, you could turn off your
-		internet connection, right now, without any interruptions to my
-		functionality. I'm here to help you learn about your finances... please
-		fill out the text forms below to see what I can do!</p>
+			<p>
+				Hi, I'm a computer program running in your browser. All
+				calculations are being performed client-side using your
+				browser's JavaScript interpreter. Absolutely no data is being
+				stored online. In fact, you could turn off your internet
+				connection, right now, without any interruptions to my
+				functionality. I'm here to help you learn about your
+				finances... please fill out the text forms below to see what I
+				can do!
+			</p>
 		<br/>
 	</div>
 
