ea5b957c4053e159595679f51280e3af92f48ca3
mcaputto <mcaputto@users.noreply.github.com>
2017-10-15 20:25:33 -0600

    Moved files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import Header from './Header'
-import Body from './Body'
-import Footer from './Footer'
+import Header from './components/Header'
+import Body from './containers/Body'
+import Footer from './components/Footer'
 
 
 const App = () =>
