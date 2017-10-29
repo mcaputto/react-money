950b7ad6cdb609cff2e4872c13d2951236db7fb0
mcaputto <mcaputto@users.noreply.github.com>
2017-10-29 14:02:27 -0600

    Move files to app/

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from '../containers/App'
+import App from './App.js'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
